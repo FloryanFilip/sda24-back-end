5a2d252bef83b098500371666eff76d269e6a7df
vonpredel <ostrafi@gmail.com>
2017-03-04 09:42:47 +0100

    merge

--- src/main/java/sda24/configuration/Config.java
+++ src/main/java/sda24/configuration/Config.java
@@ -2,24 +2,32 @@ package sda24.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import sda24.HWController;
+import sda24.dbConnector.DbConnector;
 import sda24.query.QueryDao;
 
 /**
  * Created by RENT on 2017-03-01.
  */
 
+
 @Configuration
 @ComponentScan(basePackageClasses = {HWController.class, QueryDao.class})
 @EnableWebMvc
+@Import(DbConnector.class)
+@EnableTransactionManagement
 public class Config extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*").allowedMethods("*").allowedOrigins("*");
     }
+
+
 
 }
